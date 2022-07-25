11f3a1b16ce820d7f526beb27f047b360654afdc
David Ta <david.ta555@gmail.com>
2022-07-25 16:22:42 -0400

    Added Reactstrap to Users Cards

--- frontend/src/components/UserCard.jsx
+++ frontend/src/components/UserCard.jsx
@@ -1,22 +1,38 @@
 import React from "react";
+import {
+  Card,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  CardImg,
+  Button,
+} from "reactstrap";
 
-
-
-function UserCard ({users}) {
-
-
-    return (
-        <div className="infoCard">
-            <div className="info-card-name">
-            <h1>Name: {users.first_name} </h1>
-            </div>
-            <div className="info-card-username">
-            <h3>Username: {users.username} </h3>
-            </div>
-            <div className="info-card-profile-pic">
-            <img src = {users.profile_pic} alt={users.username}/>
-            </div>
-        </div>
-    )
+function UserCard({ users }) {
+  return (
+    <Card
+      style={{
+        width: "18rem",
+      }}
+    >
+      <CardBody>
+        <CardImg
+          alt={users.username}
+          src={users.profile_pic}
+          top
+          width="100%"
+        />
+        <CardTitle tag="h5">
+          {users.first_name} {users.last_name}
+        </CardTitle>
+        <CardSubtitle className="mb-2 text-muted" tag="h6">
+          {users.username}
+        </CardSubtitle>
+        <Button color="primary" href={`/user/${users.id}`}>
+          Details
+        </Button>
+      </CardBody>
+    </Card>
+  );
 }
 export default UserCard;
--- frontend/src/pages/UserPage.jsx
+++ frontend/src/pages/UserPage.jsx
@@ -1,47 +1,43 @@
-//import { response } from 'express';
-import React, { useState, useEffect } from 'react'
-import Loader from '../components/Loader';
-// import User from '../../../backend/src/models/User'
-// import { get } from '../../../backend/src/routes/users';
-import UserCard from '../components/UserCard'
-
+import React, { useState, useEffect } from "react";
+import Loader from "../components/Loader";
+import UserCard from "../components/UserCard";
+import { Row } from "reactstrap";
 
 function UserPage() {
+  const [allUsers, setAllUsers] = useState([]);
 
-    const [ allUsers, setAllUsers ] = useState([]);
+  const getData = async () => {
+    const response = await fetch("http://localhost:8000/api/users", {
+      headers: {
+        SameSite: "None",
+      },
+    });
 
-    const getData = async () => {
-        const response = await fetch('http://localhost:8000/api/users', {
-            headers :{
-                'SameSite': 'None'
-            }
-        })
+    const data = await response.json();
 
-        const data = await response.json();
+    console.log("Data: ", data);
+    const { users } = data;
+    setAllUsers(users);
+    console.log("Users: ", allUsers);
+  };
 
-        console.log('Data: ', data);
-        const { users } = data;
-        setAllUsers(users);
-        console.log('Users: ', allUsers);
-    };
-
-    useEffect(() => {
-        getData();
-        // eslint-disable-next-line
-    }, []);
+  useEffect(() => {
+    getData();
+    // eslint-disable-next-line
+  }, []);
 
   return (
-    <div className='users'>
-        <h1>Users: </h1>
-        <div className='container'>
-             {allUsers.length > 0 ? allUsers.map((users) => <UserCard key={ users.id } users={ users } />) 
-        : <Loader />}  
-            
-        
-        </div>
-        
+    <div className="container">
+      <h1>Users: </h1>
+      <Row>
+        {allUsers.length > 0 ? (
+          allUsers.map((users) => <UserCard key={users.id} users={users} />)
+        ) : (
+          <Loader />
+        )}
+      </Row>
     </div>
-  )
+  );
 }
 
 export default UserPage;
